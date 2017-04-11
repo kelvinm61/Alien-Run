72053222ac7b323c71d87ebd61a191f96042f1c9
KelvinM61 <g00311695@gmit.ie>
2017-04-11 10:45:31 +0100

    First commit of Alien run
    First commit of the game alien run, have a functioning moving character

---
+++ Running Alien/Assets/code/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float movePace;
+	public float jumpHeight;
+
+	private Rigidbody2D rigidBody;
+
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rigidBody.velocity = new Vector2 (movePace, rigidBody.velocity.y);
+
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0))
+		{
+			rigidBody.velocity = new Vector2 (rigidBody.velocity.x, jumpHeight);
+		}
+	}
+}
