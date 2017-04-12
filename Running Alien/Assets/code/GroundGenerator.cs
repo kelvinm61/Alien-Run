e765516763181219e617eaf23e226114d3d37e32
KelvinM61 <g00311695@gmit.ie>
2017-04-12 15:49:29 +0100

    ground being created and destroyed
    I have now made an infinite loop of platforms to generate in front of
    the character and also destroy the ones behind him so as to save space.

--- Running Alien/Assets/code/GroundGenerator.cs
+++ Running Alien/Assets/code/GroundGenerator.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class GroundGenerator : MonoBehaviour {
 
+	public GameObject theGround;
+	public Transform generateMark;
+	public float distanceBetween;
+
+	private float groundWidth;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		//
+		groundWidth = theGround.GetComponent<BoxCollider2D>().size.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// if this is true it will create new platforms, it will also stop creating them past that mark.
+		if (transform.position.x < generateMark.position.x) 
+		{
+			//moved the platform widht, and left space in between with this line making sure of no overlapping..
+			transform.position = new Vector3 (transform.position.x + groundWidth + distanceBetween, transform.position.y, transform.position.z);
+		//creates copy of the ground
+			Instantiate (theGround, transform.position, transform.rotation);
+		}
 		
 	}
 }
---
+++ Running Alien/Assets/code/RemoveGround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveGround : MonoBehaviour {
+
+	public GameObject removeGroundMark;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//game object.find finds any object inside the quotes
+		removeGroundMark = GameObject.Find ("RemoveGroundMark");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//if the mark is far enough behind us we will get rid of it so we can save memory.
+		if (transform.position.x < removeGroundMark.transform.position.x) 
+		{
+			Destroy (gameObject);
+		}
+
+	}
+}
