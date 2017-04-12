61f15f1273d79e9ee628130ea4443567456246af
KelvinM61 <g00311695@gmit.ie>
2017-04-12 16:12:36 +0100

    random width between platforms
    I also changed how I created and destroyed platforms so now there is no
    platform flying above the camera out of view

--- Running Alien/Assets/code/GroundGenerator.cs
+++ Running Alien/Assets/code/GroundGenerator.cs
@@ -10,7 +10,8 @@ public class GroundGenerator : MonoBehaviour {
 
 	private float groundWidth;
 
-
+	public float distanceBetweenMin;
+	public float distanceBetweenMax;
 	// Use this for initialization
 	void Start () {
 		//
@@ -19,12 +20,17 @@ public class GroundGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
 		// if this is true it will create new platforms, it will also stop creating them past that mark.
 		if (transform.position.x < generateMark.position.x) 
-		{
-			//moved the platform widht, and left space in between with this line making sure of no overlapping..
+		{   
+			//generate random number for gaps
+			distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
+			
+				//moved the platform widht, and left space in between with this line making sure of no overlapping..
 			transform.position = new Vector3 (transform.position.x + groundWidth + distanceBetween, transform.position.y, transform.position.z);
-		//creates copy of the ground
+		
+				//creates copy of the ground
 			Instantiate (theGround, transform.position, transform.rotation);
 		}
 		
