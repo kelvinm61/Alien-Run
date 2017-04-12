8e79fbdfb5e05a5f4bc4adf673021a4b8dd86cc2
KelvinM61 <g00311695@gmit.ie>
2017-04-12 14:55:26 +0100

    Alien is animated
    Alien is animated and moves differently when doing different actions
    like running and jumping, in the process of trying to make an infinite
    loop for the ground so i don’t have to keep creating platforms.

---
+++ Running Alien/Assets/code/CameraCode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCode : MonoBehaviour {
+
+	public PlayerControl alien;
+	//vector 3 has 3 values, xyz.
+	private Vector3 alienPostion;
+	private float movingDistance;
+
+	// Use this for initialization
+	void Start () {
+		alien = FindObjectOfType<PlayerControl> (); 
+		//vector xyz
+		alienPostion = alien.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//simple math to show how far to move with the camera.
+		movingDistance = alien.transform.position.x - alienPostion.x;
+		//making camera move from left to right but not up and down.
+		transform.position = new Vector3 (transform.position.x + movingDistance, transform.position.y, transform.position.z);
+			
+		//vector xyz
+		alienPostion = alien.transform.position;
+	}
+}
--- Running Alien/Assets/code/PlayerControl.cs
+++ Running Alien/Assets/code/PlayerControl.cs
@@ -9,19 +9,43 @@ public class PlayerControl : MonoBehaviour {
 
 	private Rigidbody2D rigidBody;
 
+	public bool ground;
+	public LayerMask whatIsGround;
+
+	private Collider2D collider;
+
+	private Animator animator;
+
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D> ();
+
+		collider = GetComponent<Collider2D> ();
+
+		animator = GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
-	void Update () {
-		rigidBody.velocity = new Vector2 (movePace, rigidBody.velocity.y);
+	void Update () 
+	{
+		// to see if collider(box around objects) is touching any other collider on a particular layer t/f
 
-		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0))
+		ground = Physics2D.IsTouchingLayers(collider, whatIsGround);
+
+		rigidBody.velocity = new Vector2 (movePace, rigidBody.velocity.y);
+		// buttons used to make alien jump
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) 
 		{
-			rigidBody.velocity = new Vector2 (rigidBody.velocity.x, jumpHeight);
+			// wont allow jump until this becomes true. vector2 has 2 values, xy.
+			if (ground) 
+			{
+				rigidBody.velocity = new Vector2 (rigidBody.velocity.x, jumpHeight);
+			}
 		}
+		//sets speed so animation can happen when speed is more than 0.
+		animator.SetFloat ("speed", rigidBody.velocity.x);
+		animator.SetBool ("ground", ground);
+
 	}
 }
