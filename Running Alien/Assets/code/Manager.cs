e5d747cec7105a9b301092053660395e2b5979a6
KelvinM61 <g00311695@gmit.ie>
2017-04-24 14:46:47 +0100

    Score system back up and working
    I have rewritten the score system again and it is working along with
    holding the high score.

--- Running Alien/Assets/code/Manager.cs
+++ Running Alien/Assets/code/Manager.cs
@@ -10,10 +10,12 @@ public class Manager : MonoBehaviour {
 	public PlayerControl player;
 	private Vector3 playerStartMark;
 
+	private Scorekeeper theScorekeeper;
 	// Use this for initialization
 	void Start () {
 		groundStartPoint = groundGenerator.position;
 		playerStartMark = player.transform.position; 
+		theScorekeeper = FindObjectOfType<Scorekeeper> ();
 	}
 	
 	// Update is called once per frame
@@ -28,6 +30,8 @@ public class Manager : MonoBehaviour {
 
 	public IEnumerator RestartGameCo()
 	{
+		// score stop 
+		theScorekeeper.scoreIncrease = false;
 		//makes the player inactive
 		player.gameObject.SetActive (false);
 		//delay
@@ -36,6 +40,9 @@ public class Manager : MonoBehaviour {
 		groundGenerator.position = groundStartPoint;
 		//makes player active again
 		player.gameObject.SetActive (true);
+		//restarts the score at 0.
+		theScorekeeper.scoreCount = 0;
+		theScorekeeper.scoreIncrease = true;
 	}
 
 
---
+++ Running Alien/Assets/code/Scorekeeper.cs
@@ -0,0 +1,44 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scorekeeper : MonoBehaviour {
+
+	public Text score;
+	public Text highscore;
+
+	public float scoreCount;
+	public float highscoreCount;
+
+	public float pointpersec;
+	public bool scoreIncrease;
+	// Use this for initialization
+	void Start () { 
+		
+		//to show highscore if one has already been set.
+		if (PlayerPrefs.HasKey("Highscore")) {
+			highscoreCount = PlayerPrefs.GetFloat ("Highscore");
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//adds score per second
+		if (scoreIncrease) {
+			
+			scoreCount += pointpersec * Time.deltaTime;
+		}
+		if (scoreCount > highscoreCount)
+		{
+			highscoreCount = scoreCount;
+			//set high score and it to be saved
+			PlayerPrefs.SetFloat ("Highscore", highscoreCount);
+		}
+		//adds score to screen ....mathf.round rounds score to whole number
+		score.text = "Score: " + Mathf.Round (scoreCount);
+		highscore.text = "Highscore: " + Mathf.Round (highscoreCount);
+		
+	}
+}
